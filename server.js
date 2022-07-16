85aed7dba9137af2e15cc26f3f27c41eb9682a40
Amitash <amitu895@gmail.com>
2022-07-16 22:34:01 +0530

    Added everything, need to test, (bash script works)

--- server.js
+++ server.js
@@ -4,9 +4,27 @@ const path = require('path');
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const { exec } = require("child_process");
 const io = new Server(server);
 const port = 8080;
-let data = {'temp': 1, 'cpu': 4, 'ram': 9};
+let data = {'temp': 0, 'cpu': 0, 'ram': 0};
+
+function get_data() {
+	exec("bash data.sh", (error, stdout, stderr) => {
+	    if (error) {
+	        console.log(`error: ${error.message}`);
+	        return;
+	    }
+	    if (stderr) {
+	        console.log(`stderr: ${stderr}`);
+	        return;
+	    }
+	    var out = stdout.split(';');
+	    data.temp = out[0];
+	    data.cpu = out[1];
+	    data.ram = out[2];
+	});
+}
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -19,7 +37,7 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
-  setInterval(()=>{io.emit('msg', data);}, 1000);
+  setInterval(()=>{get_data(); io.emit('msg', data);}, 1000);
 });
 
 server.listen(port, () => {
