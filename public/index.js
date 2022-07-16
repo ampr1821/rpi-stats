a823b33a0ae822494d0654e52dbcb7fb8ff656e1
Amitash <amitu895@gmail.com>
2022-07-16 22:34:01 +0530

    Basic gauge meter - working

--- public/index.js
+++ public/index.js
@@ -4,7 +4,13 @@ var cpu = document.getElementById('cpu');
 var ram = document.getElementById('ram');
 
 socket.on('msg', (msg)=>{
-  temp.textContent = msg.temp;
-  cpu.textContent = msg.cpu;
-  ram.textContent = msg.ram;
+  document.getElementById('temp_value').textContent = 'Temp: ' + msg.temp + '°C';
+  document.getElementById('cpu_value').textContent = 'CPU: ' + msg.cpu + '%';
+  document.getElementById('ram_value').textContent = 'RAM: ' + msg.ram + '%';
+  var temp_ = msg.temp * (100 / 180);
+  var cpu_ = msg.cpu * (100 / 180);
+  var ram_ = msg.ram * (100 / 180);
+  temp.style.setProperty('--rotation', temp_ + 'deg');
+  cpu.style.setProperty('--rotation', cpu_ + 'deg');
+  ram.style.setProperty('--rotation', ram_ + 'deg');
 });
