d6f55adbbaa7da68219a03a1e5883b76e53b6cd4
Amitash <amitu895@gmail.com>
2022-07-18 18:17:15 +0530

    Fixed degree calculation and corrected command to get CPU usage

--- data.sh
+++ data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 temp=$(vcgencmd measure_temp | cut -f 2 -d = | tr -d \'C)
-cpu=$(cat /proc/stat |grep cpu |head -1|awk '{print ($5*100)/($2+$3+$4+$5+$6+$7+$8+$9+$10)}'|awk '{printf "%.1f",100-$1}')
+cpu=$[100-$(vmstat 1 2|tail -1|awk '{print $15}')]
 ram=$(free | grep Mem | awk '{printf "%.1f",$3/$2 * 100.0}')
 
 echo $temp\;$cpu\;$ram
--- public/index.js
+++ public/index.js
@@ -7,9 +7,9 @@ socket.on('msg', (msg)=>{
   document.getElementById('temp_value').textContent = 'Temp: ' + msg.temp + '°C';
   document.getElementById('cpu_value').textContent = 'CPU: ' + msg.cpu + '%';
   document.getElementById('ram_value').textContent = 'RAM: ' + msg.ram + '%';
-  var temp_ = msg.temp * (100 / 180);
-  var cpu_ = msg.cpu * (100 / 180);
-  var ram_ = msg.ram * (100 / 180);
+  var temp_ = msg.temp * 1.8;
+  var cpu_ = msg.cpu * 1.8;
+  var ram_ = msg.ram * 1.8;
   temp.style.setProperty('--rotation', temp_ + 'deg');
   cpu.style.setProperty('--rotation', cpu_ + 'deg');
   ram.style.setProperty('--rotation', ram_ + 'deg');
